371d85423a9002ff6fa60da4447e812fafde0737
DandiIndraWijaya <dandiindra29@gmail.com>
2021-05-12 09:35:51 +0700

    rename components file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,12 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import app from './app';
-import './components/skipToContent';
-import './components/navbar';
-import './components/drawer';
-import './components/hero';
-import './components/title';
-import './components/content';
-import './components/footer';
+import './components/skip-to-content';
+import './components/nav-bar';
+import './components/drawer-nav';
+import './components/restoplace-hero';
+import './components/page-title';
+import './components/page-content';
+import './components/restoplace-footer';
 
 app();

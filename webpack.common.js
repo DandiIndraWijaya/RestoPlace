baf13319b888b62bad1019ecec12064458a2163e
DandiIndraWijaya <dandiindra29@gmail.com>
2021-05-14 11:32:11 +0700

    add sass-loader package

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,20 @@ module.exports = {
         ],
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
